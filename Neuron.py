489dbe8b8ba35504dee0d4c0f328a70ee3885e74
Satori <shakeebmajid@ufl.edu>
2018-01-21 23:19:04 -0500

    added in sigmoid to be able to represent probabilities and output function that implements sigmoid for neurons on the output layer this was done so that the cross entropy function does not reach undefined values

--- Neuron.py
+++ Neuron.py
@@ -19,8 +19,21 @@ class Neuron:
 
         return self.ReLu(weightedSum)
 
+    def sigmoid(self, x):
+        return 1 / (1 + math.exp(-x))
+
+
+    def output(self, inputs, weights):
+        weightedSum = 0
+        i = 0
+        for weight in weights:
+            weightedSum += inputs[i] * weight
+            i += 1
+
+        return self.sigmoid(weightedSum)
+        
     def crossEntropy(self, inputs, weights, target):
-        estimate = process(inputs, weights)
+        estimate = self.output(inputs, weights)
         crossEntropy = target * math.log(estimate) + (1 - target) * math.log(1 - estimate)
-                
+
         return crossEntropy
