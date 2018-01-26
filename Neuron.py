547f9398bf0cd632dcdd04443731fcb28cff17ad
Satori <shakeebmajid@ufl.edu>
2018-01-25 23:05:33 -0500

    added derivative of sigmoid for use in back propagation

--- Neuron.py
+++ Neuron.py
@@ -12,29 +12,33 @@ class Neuron:
 
     def process(self, inputs, weights):
         weightedSum = 0
-        i = 0
-        for weight in weights:
-            weightedSum += inputs[i] * weight
-            i += 1
+        #i = 0
+        for Input, weight in zip(inputs, weights):
+            #weightedSum += inputs[i] * weight
+            weightedSum += Input * weight
+            #i += 1
 
         return self.ReLu(weightedSum)
 
     def sigmoid(self, x):
         return 1 / (1 + math.exp(-x))
 
+    def dSigmoid(self, x):
+        return self.sigmoid(x) * (1 - self.sigmoid(x))
 
     def output(self, inputs, weights):
         weightedSum = 0
-        i = 0
-        for weight in weights:
-            weightedSum += inputs[i] * weight
-            i += 1
+        #i = 0
+        for Input, weight in zip(inputs, weights):
+            #weightedSum += inputs[i] * weight
+            weightedSum += Input * weight
+            #i += 1
 
         return self.sigmoid(weightedSum)
 
     def crossEntropy(self, inputs, weights, target):
         estimate = self.output(inputs, weights)
-        
+
         crossEntropy = -(target * math.log(estimate) + (1 - target) * math.log(1 - estimate))
 
         return crossEntropy
